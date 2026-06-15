764d42433419ff639b5d3e2ef63f0a45c34d8271
OmarAbdo <omargamer10a@gmail.com>
2026-06-15 17:44:21 +0300

    add NullBitMap

--- src/Table/RowSerializer.cpp
+++ src/Table/RowSerializer.cpp
@@ -6,19 +6,35 @@
 #include <cstring>
 #include <stdexcept>
 
+
+// convert the row into bytes
 std::vector<uint8_t> RowSerializer::Serialize(const DbRow& row, const std::vector<ColumnDefinition>& columns, int rowSizeBytes) {
 
 
     std::vector<uint8_t> buffer(rowSizeBytes, 0);
 
-    for (const auto& col : columns) {
+    // row -> [row bytes][10 byte systems]
+    // 10 byte ->  [9 bytes for NULL][delete flag]
 
+    int footerStart = rowSizeBytes - 10;
+    int nullBitmapStart = footerStart;        // 9 bytes from footerStart to footerStart+8 , last byte for delete
+
+    // 9 bytes then 72 bit
+    uint8_t nullBitmap[9] = {0};
+
+    for (int colIdx=0;colIdx<columns.size();colIdx++) {
+        auto &col = columns[colIdx];
         std::span<uint8_t> slice = std::span<uint8_t>(buffer).subspan(col.Offset, col.Size);
 
         auto it = row.find(col.Name);
+        bool isNull = (it == row.end() || std::holds_alternative<std::monostate>(it->second));
 
-        if (it == row.end())
+        if (isNull) {
+            int bytePos = colIdx/8;
+            int bitPos = colIdx%8;
+            nullBitmap[bytePos]|=(1LL<<bitPos);
             continue;
+        }
 
         const DbObject& value = it->second;
 
@@ -61,13 +77,35 @@ std::vector<uint8_t> RowSerializer::Serialize(const DbRow& row, const std::vecto
             }
         }
     }
+
+    std::memcpy(buffer.data() + nullBitmapStart, nullBitmap, 9);
+
     return buffer;
 }
 
+
+// convert bytes into DBRow
 DbRow RowSerializer::Deserialize(std::span<const uint8_t> data, const std::vector<ColumnDefinition>& columns) {
     DbRow row;
 
-    for (const auto& col : columns) {
+    int rowSize = static_cast<int>(data.size());
+    int footerStart = rowSize - 10;
+    int nullBitmapStart = footerStart;               // 9 bytes
+
+    // Read the 9-byte null bitmap
+    const uint8_t* nullBitmap = data.data() + nullBitmapStart;
+
+    for (size_t colIdx = 0; colIdx < columns.size(); ++colIdx) {
+        const auto& col = columns[colIdx];
+        int bytePos = colIdx / 8;
+        int bitPos = colIdx % 8;
+        bool isNull = (nullBitmap[bytePos] & (1LL << bitPos)) != 0;
+
+        if (isNull) {
+            row[col.Name] = std::monostate{};
+            continue;
+        }
+
         std::span<const uint8_t> slice = data.subspan(col.Offset, col.Size);
 
         if (col.Type == "int") {
@@ -123,9 +161,9 @@ std::string RowSerializer::ReadString(std::span<const uint8_t> s) {
 }
 
 bool RowSerializer::IsDeleted(std::span<const uint8_t> data, int rowSizeBytes) {
-    return data[rowSizeBytes - 10] == 1;
+    return data[rowSizeBytes - 1] == 1;
 }
 
 void RowSerializer::MarkDeleted(std::span<uint8_t> data, int rowSizeBytes) {
-    data[rowSizeBytes - 10] = 1;
+    data[rowSizeBytes - 1] = 1;
 }
